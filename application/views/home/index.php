9f41d19eab8be83089a06812610670e1e3fcc7a2
Equan Pr <eq@junwatu.com>
2012-08-19 04:58:20 +0700

    change index content n filename

---
+++ application/views/home/index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>Laravel: A Framework For Web Artisans</title>
+	<meta name="viewport" content="width=device-width">
+	{{ HTML::style('laravel/css/style.css') }}
+</head>
+<body>
+	<div class="wrapper">
+		<header>
+			<h1>Kontak Laravel</h1>
+			<h2>Web Application For Your Contact Data</h2>
+
+			<p class="intro-text" style="margin-top: 45px;">
+			</p>
+		</header>
+		<div role="main" class="main">
+			<div class="home">
+			</div>	
+		</div>
+	</div>
+</body>
+</html>
