6294b1742e52bce870f466ad2bbd03fd66250e26
Equan Pr <eq@junwatu.com>
2012-08-20 10:11:05 +0700

    add login and registered views

---
+++ application/views/home/login.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>Start Laravel: A Framework For Web Artisans</title>
+	<meta name="viewport" content="width=device-width">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<div class="wrapper">
+		<header>
+				<h2>Start Laravel</h2>
+		</header>
+		<div class="main">
+			    <h4>Login</h4>
+			    <?php
+				echo Form::open(URL::to('user/login'));
+				?>
+				<?php
+				echo Form::label('username','Username : ');
+				echo Form::text('username');
+				?>
+
+				<?php
+				echo Form::label('password','Password : ');
+				echo Form::text('password');
+
+				echo Form::submit("Login");
+				echo Form::close();
+				?>
+		</div>
+	</div>
+</body>
+</html>
---
+++ application/views/home/registered.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>Start Laravel: A Framework For Web Artisans</title>
+	<meta name="viewport" content="width=device-width">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<div class="wrapper">
+		<header>
+				<h2>Start Laravel</h2>
+		</header>
+		<div class="main">
+			<p>Registered Page! <a href="<?php echo URL::to('/user/logout');?>">Logout</a></p>
+		</div>
+	</div>
+</body>
+</html>
